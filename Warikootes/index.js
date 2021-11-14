c0bf4cada7d9ad81d579ffb9f92888c52f655fad
GofindGod <87580981+GofindGod@users.noreply.github.com>
2021-11-14 02:12:37 -0800

    Add files via upload

---
+++ Warikootes/index.js
@@ -0,0 +1,90 @@
+const Discord = require("discord.js")
+const fetch = require("node-fetch")
+const keepAlive = require("./server")
+const Database = require("@replit/database")
+
+const db = new Database()
+const client = new Discord.Client()
+
+const sadWords = ["sad", "depressed", "unhappy", "angry"]
+
+const starterEncouragements = [
+  "Cheer up!",
+  "Hang in there.",
+  "You are a great person / bot!"
+]
+
+db.get("encouragements").then(encouragements => {
+  if (!encouragements || encouragements.length < 1) {
+    db.set("encouragements", starterEncouragements)
+  }
+})
+
+db.get("responding").then(value => {
+  if (value == null) {
+    db.set("responding", true)
+  }
+})
+
+function updateEncouragements(encouragingMessage) {
+  db.get("encouragements").then(encouragements => {
+    encouragements.push([encouragingMessage])
+    db.set("encouragements", encouragements)
+  })
+}
+
+function deleteEncouragement(index) {
+  db.get("encouragements").then(encouragements => {
+    if (encouragements.length > index) {
+      encouragements.splice(index, 1)
+      db.set("encouragements", encouragements)
+    }
+  })  
+}
+
+function getQuote() {
+  return fetch("https://zenquotes.io/api/random")
+    .then(res => {
+      return res.json()
+    })
+    .then(data => {
+      return data[0]["q"] + " -" + data[0]["a"]
+    })
+}
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on("message", msg => {
+  if (msg.author.bot) return
+
+  if (msg.content === "$inspire") {
+    getQuote().then(quote => msg.channel.send(quote))
+  }
+
+  db.get("responding").then(responding =>{
+    if (responding && sadWords.some(word => msg.content.includes(word))) {
+      db.get("encouragements").then(encouragements => {
+        const encouragement = encouragements[Math.floor(Math.random() * encouragements.length)]
+        msg.reply(encouragement)
+      })
+    }
+  })
+
+  if (msg.content.startsWith("$responding")) {
+    value = msg.content.split("$responding ")[1]
+
+    if (value.toLowerCase() == "true") {
+      db.set("responding", true)
+      msg.channel.send("Responding is on.")
+    } else {
+       db.set("responding", false)
+      msg.channel.send("Responding is off.")     
+    }
+  }
+
+})
+
+keepAlive()
+client.login('ODcyODQ2NTYxNzIwMjk1NDg0.YQvztQ.lWGKKTVi3tzNuhNeN3hFg-B6dWA')
